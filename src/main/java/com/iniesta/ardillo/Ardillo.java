db40b2875dd72146ddaf583555efc1e012e2f0bb
Antonio J. Iniesta <antonioj.iniesta@gmail.com>
2014-02-21 01:30:43 +0100

    Initial draft of the GUI and launcher

---
+++ src/main/java/com/iniesta/ardillo/Ardillo.java
@@ -0,0 +1,23 @@
+package com.iniesta.ardillo;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.*;
+
+
+public class Ardillo {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+
+    @FXML
+    void initialize() {
+
+
+    }
+
+}
---
+++ src/main/java/com/iniesta/ardillo/RunArdillo.java
@@ -0,0 +1,24 @@
+package com.iniesta.ardillo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class RunArdillo extends Application{
+
+	public static void main(String[] args) throws Exception{
+		launch(args);
+	}
+
+	public void start(Stage stage) throws Exception{
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/iniesta/ardillo/Ardillo.fxml"));
+		Parent parent = (Parent) loader.load();
+		// Create the scene
+		Scene scene = new Scene(parent, 900, 600);
+		stage.setScene(scene);
+		// Show the stage
+		stage.show();
+	}
+}
