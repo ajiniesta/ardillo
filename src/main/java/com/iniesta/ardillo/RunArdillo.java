f9fc509f94845ec72492af9ac508dba2445a8243
Antonio J. Iniesta <antonioj.iniesta@gmail.com>
2014-12-30 23:39:08 +0100

    Adding log4j properties, also in the assembly

--- src/main/java/com/iniesta/ardillo/RunArdillo.java
+++ src/main/java/com/iniesta/ardillo/RunArdillo.java
@@ -8,29 +8,43 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class RunArdillo extends Application{
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-	public static void main(String[] args) throws Exception{
+public class RunArdillo extends Application {
+
+	private final static Logger logger = LoggerFactory.getLogger("ARDILLO");
+
+	public static void main(String[] args) throws Exception {
+		logger.info("Welcome!");
 		launch(args);
 	}
 
-	public void start(Stage stage) throws Exception{
+	public void start(Stage stage) throws Exception {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/iniesta/ardillo/Ardillo.fxml"));
 		Parent parent = (Parent) loader.load();
 		// Create the scene
 		Scene scene = new Scene(parent, 900, 600);
 		stage.setScene(scene);
 		// Show the stage
-		stage.show();		
+		stage.show();
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see javafx.application.Application#stop()
 	 */
 	@Override
 	public void stop() throws Exception {
-		super.stop();
-		HibernateUtil.getSessionFactory().close();
-	}	
-	
+		try {
+			super.stop();
+			if (!HibernateUtil.getSessionFactory().isClosed()) {
+				HibernateUtil.getSessionFactory().close();
+			}
+		} finally {
+			logger.info("Bye Bye");
+		}
+	}
+
 }
